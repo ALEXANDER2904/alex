ff3607a2716620a1673dee20f749777915f1b8ba
victor-losada <victorlosada2211@gmail.com>
2023-10-02 11:46:43 -0500

    alex

--- js/carrito.js
+++ js/carrito.js
@@ -1,18 +1,30 @@
 let numRestar = document.getElementById("numRestar");
 let numView = document.getElementById("numView");
 let numSumar = document.getElementById("numSumar");
+let canProductis = document.getElementById("cantProductos");
+let botonEliminar = document.getElementById("eliminar"); 
+/*Simulacion de valor entero proveniente de una BD*/
+let valorUnitarioProducto = 35000;
+let valorUnitarioProductoStr = new Intl.NumberFormat('de-DE').format(valorUnitarioProducto);
+
+document.getElementById("valorUnitarioProducto").innerText = valorUnitarioProductoStr;
 
 numRestar.addEventListener("click", function(){
     let aux = numView.innerHTML.toString();
     let auxInt = parseInt(aux);
-    if (auxInt) {
+    if (auxInt > 0) {
         auxInt--;
         document.getElementById("numView").innerText = auxInt.toString();
+        if (auxInt == 1) {
+            document.getElementById("cantProductos").innerHTML = auxInt.toString() + " producto.";
+        }
+        else if (auxInt == 0 || auxInt > 1) {
+            document.getElementById("cantProductos").innerHTML = auxInt.toString() + " productos.";
+        } 
     }
-})
-numView.addEventListener("click", function(){
-    let aux = numView.innerHTML.toString();
-    alert (aux);
+    let auxSubTotal = auxInt * valorUnitarioProducto;
+    let auxSubTotalStr = new Intl.NumberFormat('de-DE').format(auxSubTotal)
+    document.getElementById("valorSubtotal").innerHTML = auxSubTotalStr;
 })
 numSumar.addEventListener("click", function(){
     let aux = numView.innerHTML.toString();
@@ -20,5 +32,20 @@ numSumar.addEventListener("click", function(){
     if (auxInt < 10) {
         auxInt++;
         document.getElementById("numView").innerText = auxInt.toString();
-    }
+        if (auxInt == 1) {
+            document.getElementById("cantProductos").innerHTML = auxInt.toString() + " producto.";
+        }
+        else if (auxInt == 0 || auxInt > 1) {
+            document.getElementById("cantProductos").innerHTML = auxInt.toString() + " productos.";
+        }
+}
+    let auxSubTotal = auxInt * valorUnitarioProducto;
+    let auxSubTotalStr = new Intl.NumberFormat('de-DE').format(auxSubTotal)
+    document.getElementById("valorSubtotal").innerHTML = auxSubTotalStr;
+})
+//ELIMINAR BOTON
+botonEliminar.addEventListener("click", function(){
+    document.getElementById("numView").innerHTML = "0";
+    document.getElementById("cantProductos").innerHTML = "0 productos";
+    document.getElementById("valorSubtotal").innerHTML = "0";
 })
---
+++ js/mercadolibre.js
@@ -0,0 +1,12 @@
+class Automovil {
+    constructor(marca, modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+}
+
+let auto1 = new Automovil("Pagani", "F2");
+
+alert(auto1.marca + " " + auto1.modelo);
+
+inputBusqueda.addEventListener("keydown", function)
